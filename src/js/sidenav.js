5ff1dda6444b227f29166adafbf11f95b64c96a6
hamhwong <hamhwong@gmail.com>
2017-08-16 21:28:42 +0800

    today's final update

--- src/js/sidenav.js
+++ src/js/sidenav.js
@@ -1,14 +1,14 @@
 var sideopen = function() {
   $('.sideBtn').removeClass('nav_collapse').addClass('nav_open');
   $('.sideNav').removeClass('nav_collapse').addClass('nav_open');
-  $('.sideBtn .glyphicon').removeClass().addClass('glyphicon').addClass('glyphicon-triangle-left')
+  $('.sideBtn .glyphicon').removeClass().addClass('glyphicon').addClass('sideBtnIcon-open')
   $('#right_wapper').removeClass().addClass('shrunk')
 }
 
 var sideclose = function() {
   $('.sideBtn').removeClass('nav_open').addClass('nav_collapse');
   $('.sideNav').removeClass('nav_open').addClass('nav_collapse');
-  $('.sideBtn .glyphicon').removeClass().addClass('glyphicon').addClass('glyphicon-triangle-right')
+  $('.sideBtn .glyphicon').removeClass().addClass('glyphicon').addClass('sideBtnIcon-close')
   $('#right_wapper').removeClass().addClass('extend')
 }
 
